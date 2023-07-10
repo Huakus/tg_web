75c1de595eef8eb0e0c84fbd2af582a385b5dd15
Joaquin Senn <joaquinsenn@gmail.net>
2023-07-10 09:54:04 +0200

    First working version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TownView from './components/TownView';
+
+
+function App() {
+  return (
+    <div className="App">
+      <TownView />
+    </div>
+  );
+}
+
+export default App;
